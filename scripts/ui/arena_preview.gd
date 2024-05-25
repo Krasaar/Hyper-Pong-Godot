0944650655f3de4552a4b94d1578c84f5dbbd4ab
Adam <a.ban1245@gmail.com>
2024-05-25 15:35:26 +0200

    fixed issues with scaling, corrected calculations of walls and goals size to match the size of a whole arena for arena previews, fixed a bug which was preventing preview from rotating player's to match direction they face

--- scripts/ui/arena_preview.gd
+++ scripts/ui/arena_preview.gd
@@ -4,7 +4,7 @@ var area_size : Vector2
 
 func set_background(background : Sprite2D):
 	add_child(background.duplicate())
-	var rec = background.get_rect()
+	background.scale = abs(background.scale)
 	area_size = background.texture.get_size() * background.scale
 	return
 
@@ -19,61 +19,61 @@ func set_walls(walls):
 		wall_placement.texture = WALL_SMALL
 		wall_placement.transform = wall.transform
 		wall_placement.region_enabled = true
+		wall_placement.scale = abs(wall_placement.scale)
 		
-		var goal_size = Vector2.ONE
+		var wall_size = WALL_SMALL.get_size()
 		
 		if wall_placement.transform.get_scale().aspect() < 1.0:
-			goal_size.y = int(wall_placement.scale.y) + 1
+			wall_size.y = area_size.y
 		else:
-			goal_size.x = int(wall_placement.scale.x) + 1
+			wall_size.x = area_size.x
 			
 		wall_placement.scale = Vector2.ONE
 		
-		wall_placement.region_rect.size = GOAL_NET.get_size() * goal_size
-		
+		wall_placement.region_rect.size = wall_size
 		add_child(wall_placement)
 	return
 	
 const GOAL_NET = preload("res://assets/goal_net.png")	
 const PADDLE_TEXTURE = preload("res://assets/paddle_texture.png")
 func set_team_spawns(team_spawns):
-	for team_spawn in team_spawns:
-		assert(team_spawn is TeamSpawn)
+	for ts in team_spawns:
+		assert(ts is TeamSpawn)
+		var team_spawn : TeamSpawn = ts
 		var goal_placement : Sprite2D = Sprite2D.new()
 		goal_placement.texture = GOAL_NET
 		goal_placement.transform = team_spawn.get_goal().transform
+		goal_placement.scale = abs(goal_placement.scale)
 		goal_placement.region_enabled = true
 		
-		var goal_size = Vector2.ONE
+		var goal_size = GOAL_NET.get_size()
 		
-		if goal_placement.transform.get_scale().aspect() < 1.0:
-			goal_size.y = int(goal_placement.scale.y) + 1
+		if goal_placement.scale.aspect() < 1.0:
+			goal_size.y = area_size.y
 		else:
-			goal_size.x = int(goal_placement.scale.x) + 1
+			goal_size.x = area_size.x
 			
 		goal_placement.scale = Vector2.ONE
 		
-		goal_placement.region_rect.size = GOAL_NET.get_size() * goal_size
-		# goal_placement.region_rect.size = GOAL_NET.get_size() * Vector2(int(goal_placement.scale.x), int(goal_placement.scale.y))
+		goal_placement.region_rect.size = goal_size
 		add_child(goal_placement)
 		
-		for player_spawn in team_spawn.player_spawns:
+		for ps in team_spawn.player_spawns:
+			assert(ps is PlayerSpawn)
+			var player_spawn : PlayerSpawn = ps
 			var player_placement : Sprite2D = Sprite2D.new()
 			player_placement.texture = PADDLE_TEXTURE
-			player_placement.position = player_spawn.position
+			player_placement.transform = player_spawn.transform
 			
-			match player_spawn.Direction:
+			match player_spawn.faced_direction:
 				PlayerSpawn.Direction.LEFT:
-					pass
+					player_placement.set_rotation_degrees(90)
 				PlayerSpawn.Direction.RIGHT:
-					pass
+					player_placement.set_rotation_degrees(90)
 				PlayerSpawn.Direction.UP:
-					player_placement.rotation_degrees = 90
 					pass
 				PlayerSpawn.Direction.DOWN:
-					player_placement.rotation_degrees = 90
 					pass
-					
 			add_child(player_placement)
 	return
 	
